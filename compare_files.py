b570692c77c7089d778870e65d5b58471cd0d6cd
Denys <denys.shtoma@knu.ua>
2025-03-28 10:53:32 +0200

    Implement compare_sets function

--- compare_files.py
+++ compare_files.py
@@ -5,3 +5,14 @@ def read_lines_from_file(filepath: str) -> set:
     with open(filepath, 'r', encoding='utf-8') as f:
         lines = f.read().splitlines()
     return set(lines)
+
+
+def compare_sets(set_a: set, set_b: set) -> (set, set):
+    """
+    Порівнює дві множини та повертає:
+    1) Множину спільних елементів
+    2) Множину унікальних елементів (лише в одній із множин)
+    """
+    same = set_a.intersection(set_b)
+    diff = set_a.symmetric_difference(set_b)
+    return same, diff
